20f1f2daf69497f82347087c0ea0e1ee3c671236
pierre villard <pvillard@ilki.fr>
2020-02-22 09:57:10 +0100

    Update entrypoint.sh

--- actions/build-docs/entrypoint.sh
+++ actions/build-docs/entrypoint.sh
@@ -2,4 +2,4 @@
 git clone https://github.com/ilkilab/agorakube-docs.git
 cd agorakube-docs/www
 mkdocs build --clean
-aws s3 sync /var/site/agorakube-docs/www/site/ s3://agorakube-docs/ --delete
+aws s3 sync ./site/ s3://agorakube-docs/ --delete
